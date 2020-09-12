0dafeee7f28c2ff52c840b7d03573a33c24fe228
esantofimio <Bogota1234>
2020-09-12 15:20:18 -0500

    :sparkles: threads for matrix applied

--- matrix/matrix-posix.c
+++ matrix/matrix-posix.c
@@ -1,45 +1,88 @@
 #include <stdio.h>
-#define DIMENSIONS 700
 #include <time.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <math.h>
+#define THREADS 8
+double **A;
+double **B;
+double **C;
+int dimensions = 120;
 
-void matrix_calculation(double matrix[DIMENSIONS][DIMENSIONS], double matrix_result[DIMENSIONS][DIMENSIONS]){
-    // double matrix_result[DIMENSIONS][DIMENSIONS];
+typedef struct thread_data{
+    long principio;
+    long fin; 
+} thread_data;
+
+void *matrix_calculation(void *arg){
+    thread_data *interval  = (thread_data *)arg;
     double c = 0;
-    for (int i = 0; i < DIMENSIONS; i++){
-        for (int j = 0; j < DIMENSIONS; j++){
-            for (int k = 0; k < DIMENSIONS; k++){
-                c += matrix[j][k] * matrix[k][i];
+    // COLUMNAS DE B
+    for (int i = 0; i < dimensions; i++){
+        //FILAS DE A
+        for (int j = interval->principio; j < interval->fin; j++){
+            //COLUMNAS DE A
+            for (int k = 0; k < dimensions; k++){
+                c += A[j][k] * B[k][i];
             }  
-            matrix_result[i][j] = c;
+            C[j][i] = c;
             c=0;
         }
+    }
+
+    pthread_exit(NULL);
+}
+
+void print_matrix(){
+    for (int i = 0; i < dimensions; i++){
+        for (int j = 0; j < dimensions; j++){
+            printf("%2.12f \t", C[i][j]);
+        }
+        printf("\n");
     }
 }
 
 int main(){
-    double matrix[DIMENSIONS][DIMENSIONS];
-    double matrix_result[DIMENSIONS][DIMENSIONS];
-    for (int i = 0; i < DIMENSIONS; i++){
-        for (int j = 0; j < DIMENSIONS; j++){
-            matrix[i][j] = 1e5;
-        }
+    pthread_t tids[THREADS];
+    int blocks = 0;
+    thread_data interval[THREADS];
+
+    A = (double **)malloc(dimensions * sizeof(double *));
+    B = (double **)malloc(dimensions  * sizeof(double *));
+    C = (double **)malloc(dimensions  * sizeof(double *));
+    
+    for (int i = 0; i < dimensions; i++){
+        A[i] = (double *)malloc(dimensions * sizeof(double));
+        B[i] = (double *)malloc(dimensions * sizeof(double));
+        C[i] = (double *)malloc(dimensions * sizeof(double));
     }
-    for (int i = 0; i < DIMENSIONS; i++){
-        for (int j = 0; j < DIMENSIONS; j++){
-            matrix_result[i][j] = 0;
+
+    for (int i = 0; i < dimensions; i++){
+        for (int j = 0; j < dimensions; j++){
+            A[i][j] = 1e2;
+            B[i][j] = 1e2;
         }
     }
 
+    blocks = floor(dimensions/THREADS);
     clock_t begin = clock();
-    matrix_calculation(matrix, matrix_result);
 
-    for (int i = 0; i < DIMENSIONS; i++){
-        for (int j = 0; j < DIMENSIONS; j++){
-            printf("%2.12f", matrix_result[i][j]);
+
+    for(long i = 0; i < THREADS + 1; i++){
+        interval[i].principio = ((blocks)*i);
+        if (i == THREADS){
+            interval[i].fin = ((blocks*i)+(dimensions%THREADS));
+        }else{
+            interval[i].fin = ((blocks)*(i+1));
         }
-        printf("\n");
+        pthread_create(&tids[i], NULL, matrix_calculation, &interval[i]);
     }
+
+    for(int j = 0; j < THREADS; j++){
+        pthread_join(tids[j], NULL);
+    } 
     clock_t end = clock();
+    // print_matrix();
     double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
     printf("%2.12f",time_spent);
 }
