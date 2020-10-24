9c2f08545e9f9b92e80ff7dce9beb4d11db26a58
Elmar <esantofimios@unal.edu.co>
2020-10-23 21:34:26 -0500

    :sparkles: transformed videocapture to matrix structure

--- simple-sharpen-openmp/sharpen.cpp
+++ simple-sharpen-openmp/sharpen.cpp
@@ -16,16 +16,16 @@ int video_fps, video_totalFrames;
 VideoCapture inputVideo;
 VideoWriter outputVideo;
 VideoWriter *outputVideos;
+int **videoMatrix, **matrixOutput;
 Size S;
 float KERNEL[3][3] = {{1,0,-1},{0,0,0},{-1,0,1}};
 
 
-Mat applySharpen(Mat input){
-    Mat output = input.clone();
+void applySharpen(int *input, int frameIndex){
     for(int x =0; x< S.width; x++){
         for(int y=0; y< S.height; y++){
             int loc = (x + y * S.width);
-            float blue,green,red;
+            int blue,green,red;
             blue=green=red=0;
             Vec3b pixel;
             int coeficent = 0;
@@ -36,43 +36,46 @@ Mat applySharpen(Mat input){
                     int loc_conv = (xloc + S.width*yloc);
                     if(0<=i && i<S.width-1 && 0<=j && j<S.height-1){
                         coeficent = KERNEL[i][j];
-                        pixel = input.at<Vec3b>(loc_conv);
-                        blue += pixel.val[0] * coeficent;
-                        green += pixel.val[1] * coeficent;
-                        red += pixel.val[2] * coeficent;
+                        // pixel = input.at<Vec3b>(loc_conv);
+                        blue += input[(yloc*S.width*3)+(xloc * 3) + 0] * coeficent;
+                        green += input[(yloc*S.width*3)+(xloc * 3) + 1] * coeficent;
+                        red += input[(yloc*S.width*3)+(xloc * 3) + 2] * coeficent;
                     }
                 }
             }
-            Vec3b pixelSharpen = Vec3b(blue, green, red);
-            output.at<Vec3b>(loc)= pixelSharpen;
+            matrixOutput[frameIndex][(y*S.width*3)+(x * 3) + 0] = blue;
+            matrixOutput[frameIndex][(y*S.width*3)+(x * 3) + 1] = green;
+            matrixOutput[frameIndex][(y*S.width*3)+(x * 3) + 2] = red;
         }
     }
-    return output;
 }
 
-void processFrames(int nThread, VideoCapture inputVideo) {
+void processFrames(int nThread) {
     // sabemos el hilo, numero de frames total
     unsigned long int ini = (int)(video_totalFrames/THREADS)*(nThread);
     unsigned long int fin = (int)(video_totalFrames/THREADS) + ini;
-    printf("Hilo: %d Inicio: %ld , Fin: %ld \n", nThread, ini, fin);
+    // printf("Hilo: %d Inicio: %ld , Fin: %ld \n", nThread, ini, fin);
 
     Mat src, res;
     for(int i = ini; i < fin; i++){
-        printf("indice %d \n", i);
+        // printf("indice %d \n", i);
         // inputVideo >> src;              // read
-        src = inputVideo.set(1,i);
-        if (src.empty()) break;         // check if at end
-        inputVideo.read(src);
-        imwrite("myframe"+std::to_string(i)+".jpg", src);
-        // res = applySharpen(src);
-    //    outputVideo.write(res); //save or
+        // inputVideo.set(1,i);
+        // inputVideo.read(src);
+        // if (src.empty()){
+        //     printf("Src empty");
+        // }
+        
+        // imwrite("/home/elmar/Documents/Distribuidos/distribuidos-2020/files/myframe"+std::to_string(i)+".jpg", src);
+        applySharpen(videoMatrix[i], i);
+        // outputVideo.write(res); //save or
         // outputVideo << res;
     }
 }
 
 int main(int argc, char **argv)
 {
- 
+
    // read arguments
     if ( argc != 4 )
     {
@@ -91,7 +94,7 @@ int main(int argc, char **argv)
 
     //get codec type
     int ex = static_cast<int>(inputVideo.get(CAP_PROP_FOURCC));
-    
+
     // get size of video
     S = Size((int) inputVideo.get(CAP_PROP_FRAME_WIDTH),
     (int) inputVideo.get(CAP_PROP_FRAME_HEIGHT));
@@ -99,71 +102,49 @@ int main(int argc, char **argv)
     video_fps = inputVideo.get(CV_CAP_PROP_FPS);
     video_totalFrames = inputVideo.get(7);
 
+    videoMatrix = (int **)malloc(video_totalFrames * sizeof(int *));
+    matrixOutput = (int **)malloc(video_totalFrames * sizeof(int *));
+
+    for (int i = 0; i < video_totalFrames; i++){
+        videoMatrix[i] = (int *)malloc(S.width* S.height * 3 * sizeof(int));
+        matrixOutput[i] = (int *)malloc(S.width* S.height * 3 * sizeof(int));
+    }
+
     printf("Processing video ... \n FPS: %d ,  Total Frames: %d \n", video_fps, video_totalFrames);
 
-    // video Writter
-    // outputVideos = new *VideoWriter[THREADS];
-    // for (int i=1;i<THREADS+1;i++)
-    // {
-    //     outputVideos[i-1] = new VideoWriter();
-    // }
     outputVideo.open(oFile, ex, inputVideo.get(CAP_PROP_FPS), S, true);
-     
-    // #pragma omp parallel num_threads(THREADS)
-	// {
-    int j = omp_get_thread_num();
-    processFrames(j, inputVideo);
-    //     #pragma omp barrier
-    // }
 
-
-    // Get image example
-    // Mat src, res;
-    // src = inputVideo.set(1,100);
-    // inputVideo.read(src);
-    // imwrite("myframe.jpg", src);
-    
-
-
-    // Mat src, res;
-/*     for(int i=0;i<video_totalFrames; i++) //Show the image captured in the window and repeat
-    {
-        inputVideo >> src;              // read
-        if (src.empty()) break;         // check if at end
-        res = applySharpen(src);
-       //outputVideo.write(res); //save or
-       outputVideo << res;
-    } */
-
-
-
-    // width = input.cols;
-    // height =input.rows;
-    // // define the output as a clone of input image
-    // output = input.clone();
-
-
-    // printf(" Processing image %s \n width: %d  - Heigh : %d \n",argv[1],width,height);
-
-    // printf("Kernel : %d   Number of Threads: %d \n",KERNEL,THREADS);
-    //  //launch threads
-    // pthread_t tids[THREADS+1];
-
-    // for(long i = 1; i<=THREADS; i++)
-    // {
-    //     pthread_create(&tids[i], NULL, blur, (void*)i);
-    // }
-
-    // //wait for threads...
-    // for(int k = 1; k<=THREADS; k++)
-    // {
-    //     pthread_join(tids[k], NULL);
-    // }
-
-    // //write the output image 
-    // imwrite( oFile, output );
-
-
+    #pragma omp parallel num_threads(THREADS)
+	{
+        int j = omp_get_thread_num();
+        processFrames(j);
+        #pragma omp barrier
+    }
     return 0;
 
+}
+
+void setVideoFrame(Mat frameInput, int frameIndex);
+
+int setVideoMatrix(VideoCapture inputVideo){
+    Mat src;
+    for (int i = 0; i < video_totalFrames; i++){
+        printf("indice %d \n", i);
+        inputVideo.set(1,i);
+        inputVideo.read(src);
+        if (src.empty()){
+            printf("Src empty");
+        }
+        setVideoFrame(src, i);
+    }
+}
+
+void setVideoFrame(Mat frameInput, int frameIndex){
+    for(int i=0;i<S.width;i++){
+       for(int j=0;j<S.height;j++){
+        videoMatrix[frameIndex][(j*S.width*3)+(i*3)+0]= frameInput.at<Vec3b>(i,j)[0];
+        videoMatrix[frameIndex][(j*S.width*3)+(i*3)+1]= frameInput.at<Vec3b>(i,j)[1];
+        videoMatrix[frameIndex][(j*S.width*3)+(i*3)+2]= frameInput.at<Vec3b>(i,j)[2];
+       }
+     }
 }
